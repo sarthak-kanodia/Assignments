8556df3e126a3d27a27b9c0344f6e5bc96c43f2e
sarthak-kanodia <kanodiasarthak.2k18@gmail.com>
2022-07-21 12:41:31 +0530

    MinStack Mockito Test

--- DataStructures/src/test/java/com/virtusa/DataStructures/MinStackMockTest.java
+++ DataStructures/src/test/java/com/virtusa/DataStructures/MinStackMockTest.java
@@ -1,38 +1,41 @@
 package com.virtusa.DataStructures;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.spy;
+
 import java.util.EmptyStackException;
 
 import org.junit.Test;
 
-public class MinStackTest 
+public class MinStackMockTest 
 {
+	MinStack stack1;
 	
     @Test(expected = EmptyStackException.class)
     public void top_ThrowsAnException_StackIsEmpty()
     {    	
-        MinStack stack1 = new MinStack();
+        stack1 = spy(MinStack.class);
         stack1.top();
     }
     
     @Test(expected = EmptyStackException.class)
     public void pop_ThrowsAnException_StackIsEmpty()
     {    	
-        MinStack stack1 = new MinStack();
+    	stack1 = spy(MinStack.class);
         stack1.pop();
     }
     
     @Test(expected = EmptyStackException.class)
     public void getMin_ThrowsException_IfStackIsEmpty()
     {    	
-        MinStack stack1 = new MinStack();
+    	stack1 = spy(MinStack.class);
         stack1.getMin();
     }
     
     @Test
     public void top_ReturnsLastElement_StackContainsElement()
     {    	
-        MinStack stack1 = new MinStack();
+    	stack1 = spy(MinStack.class);
         stack1.push(10);
         stack1.push(20);
         stack1.push(30);
@@ -44,7 +47,7 @@ public class MinStackTest
     @Test
     public void pop_ReturnsLastElement_StackContainsElement()
     {    	
-        MinStack stack1 = new MinStack();
+    	stack1 = spy(MinStack.class);
         stack1.push(10);
         stack1.push(20);
         stack1.push(30);
@@ -55,7 +58,7 @@ public class MinStackTest
     @Test
     public void getMin_MinElement_StackContainsElements()
     {    	
-        MinStack stack1 = new MinStack();
+    	stack1 = spy(MinStack.class);
         stack1.push(10);
         stack1.push(20);
         stack1.push(30);
